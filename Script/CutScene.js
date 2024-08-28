6a381a9580364b38472a54e67a57f8fae3620bc9
carmenmj <carmenmarvellaj@gmail.com>
2024-08-28 12:33:56 +0800

    cutscreen changes

--- Script/CutScene.js
+++ Script/CutScene.js
@@ -1,9 +1,11 @@
 // script.js
 
 const dialogues = [
-    { text: "Welcome to the world of the web game!", portrait: "/Style/source/edogawa-conan.jpg" },
-    { text: "Your journey begins now. Are you ready?", portrait: "/Style/source/edogawa-conan.jpg" },
-    { text: "Remember to check your inventory often.", portrait: "/Style/source/edogawa-conan.jpg" }
+    { text: "Oh no, it's 3p.m already and i haven't feed the dinosaur yet, they must be very hungry by now.", portrait: "/Style/source/edogawa-conan.jpg" },
+    { text: "Let's grab a bucket of meet and feed them now.", portrait: "/Style/source/edogawa-conan.jpg" },
+    { text: "So hungry, where is my food!", portrait: "/Style/source/dinoAvatar.png" },
+    { text: "RAWRRRRRRR!!!!", portrait: "/Style/source/dinoAvatar.png" },
+    { text: "AAAAHHHH Why are you so aggressive today!", portrait: "/Style/source/edogawa-conan.jpg" }
   ];
   
   let currentDialogueIndex = 0;
@@ -26,6 +28,24 @@ const dialogues = [
   
       console.log("Text set to:", text);
       console.log("Portrait set to:", portraitSrc);
+
+      document.body.style.transition = 'background 0.5s ease';
+
+      if (currentDialogueIndex === dialogues.length - 5) {
+        document.body.style.background = 'url("/Style/source/CS1emptyChair.png")'; // Change the background to an image
+      } else if (currentDialogueIndex === dialogues.length - 1) {
+        document.body.style.background = 'url("/Style/source/CS4openEnclosure.png")'; // Special background for the last dialogue
+      } else if (currentDialogueIndex === dialogues.length - 4) {
+        document.body.style.background = 'url("/Style/source/CS2meet.png")'; // Change the background to an image
+      } else if (currentDialogueIndex === dialogues.length - 3) {
+        document.body.style.background = 'url("/Style/source/CS3enclosure.png")'; // Change the background to an image
+      } else {
+        document.body.style.background = 'url("/Style/source/CS3enclosure.png")'; // Default background image
+      }
+      
+      document.body.style.backgroundRepeat = 'no-repeat';
+      document.body.style.backgroundSize = '100% 50%';
+      
     } else {
       // Automatically redirect to another HTML file after a short delay
       setTimeout(() => redirectToNewPage('nextpage.html'), 2000);  // 2000 milliseconds (2 seconds) delay

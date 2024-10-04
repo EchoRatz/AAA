3c66dbc91c8b760e9f5fa07e8a286e708df4f8e3
KanXd <Kantaphonkit@gmail.com>
2024-10-04 16:29:23 +0800

    Add resource
    - Add ending scene

--- Script/levels.js
+++ Script/levels.js
@@ -4,20 +4,26 @@ const level1 = {
     background: "Style/source/Background.png",
     enemies: [
         {
-            type: "ijo",
+            type: "Dino1",
             health: 100,
-            image: "Style/source/Dino/ijo.png"
+            image: "Style/source/Dino/Dino1.png"
         },
         {
-            type: "merah",
+            type: "Dino2",
             health: 200,
-            image: "Style/source/Dino/merah.jpg"
+            image: "Style/source/Dino/Dino2.png"
             // Add image paths if needed
         },
         {
-            type: "kuning",
+            type: "Dino3",
             health: 500,
-            image: "Style/source/Dino/kuning.png"
+            image: "Style/source/Dino/Dino3.png"
+            // Add image paths if needed
+        },
+        {
+            type: "Dino4",
+            health: 700,
+            image: "Style/source/Dino/Dino4.png"
             // Add image paths if needed
         }
     ],
@@ -27,6 +33,40 @@ const level1 = {
     ]
 };
 
-const levels = [level1];
+const level2 = {
+    levelNumber: 2,
+    background: "Style/source/Background.png",
+    enemies: [
+        {
+            type: "Dino5",
+            health: 1000,
+            image: "Style/source/Dino/Dino5.png"
+        },
+        {
+            type: "Dino6",
+            health: 1200,
+            image: "Style/source/Dino/Dino6.png"
+            // Add image paths if needed
+        },
+        {
+            type: "Dino7",
+            health: 1500,
+            image: "Style/source/Dino/Dino7.png"
+            // Add image paths if needed
+        },
+        {
+            type: "Dino8",
+            health: 2000,
+            image: "Style/source/Dino/Dino8.png"
+            // Add image paths if needed
+        }
+    ],
+    objectives: [
+        "defeat all enemies",
+        "find the hidden treasure"
+    ]
+};
+
+const levels = [level1,level2];
 
 
